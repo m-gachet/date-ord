c03c48b1fbf271bfec9a0c2504e1e4f675219e16
Marcelo Gachet <marcelo@growdnd.com>
2023-12-03 12:34:26 -0800

    alt format

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,6 @@
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 
-var now = new dayjs();
-document.getElementById("nowSpan").textContent = now.format("MMM Do [of] YYYY");
+//var now = new dayjs();
+//document.getElementById("nowSpan").textContent = now.format("MMM Do [of] YYYY");
+let today = dayjs().format('dddd, MMMM Do');
+document.getElementById("nowSpan").textContent = today;
